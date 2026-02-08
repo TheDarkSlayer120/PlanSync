87f98673a628e5cc2c3ce0c6062e0d49ba918154
mryes <rk2814240@gmail.com>
2026-02-08 16:17:51 +0000

    Add ConsoleUtils class for console input handling

---
+++ PlanSync Coding/ConsoleUtils.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ConsoleUtils {
+    public static final Scanner scanner = new Scanner(System.in);
+
+    public static void pause() {
+        System.out.println("\nPress ENTER to continue...");
+        scanner.nextLine();
+    }
+
+    public static void clearLine() {
+        System.out.print("\r");
+    }
+}
