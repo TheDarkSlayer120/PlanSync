a1c4be41bc53ad8fd4de2d972a288529777dff33
mryes <rk2814240@gmail.com>
2026-02-10 23:13:35 +0000

    Add Calendar option to PlanSync menu

--- PlanSync Coding/PlanSync.java
+++ PlanSync Coding/PlanSync.java
@@ -9,6 +9,7 @@ public class PlanSync {
                 case TIMER -> PlanSyncTimer.run();
                 case STOPWATCH -> PlanSyncStopwatch.run();
                 case TIME_CALCULATOR -> PlanSyncTimeCalculator.run();
+                case CALENDAR -> PlanSyncCalendar.run();
                 default -> Navigation.EXIT;
             };
         }
@@ -22,6 +23,7 @@ public class PlanSync {
         System.out.println("1. Timer");
         System.out.println("2. Stopwatch");
         System.out.println("3. Time Calculator");
+        System.out.println("4. Calendar");
         System.out.println("0. Exit");
         System.out.print("\nChoose option: ");
 
@@ -29,6 +31,7 @@ public class PlanSync {
             case "1" -> Navigation.TIMER;
             case "2" -> Navigation.STOPWATCH;
             case "3" -> Navigation.TIME_CALCULATOR;
+            case "4" -> Navigation.CALENDAR;
             case "0" -> Navigation.EXIT;
             default -> Navigation.MAIN;
         };
