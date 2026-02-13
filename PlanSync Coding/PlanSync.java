120e0df39ddc9a5a953d5f9782fa063af21ccf8a
mryes <rk2814240@gmail.com>
2026-02-13 00:24:29 +0000

    Add options for Active and Completed Tasks

--- PlanSync Coding/PlanSync.java
+++ PlanSync Coding/PlanSync.java
@@ -10,6 +10,8 @@ public class PlanSync {
                 case STOPWATCH -> PlanSyncStopwatch.run();
                 case TIME_CALCULATOR -> PlanSyncTimeCalculator.run();
                 case CALENDAR -> PlanSyncCalendar.run();
+                case ACTIVE_TASKS -> PlanSyncActiveTasks.run();
+                case COMPLETED_TASKS -> PlanSyncCompletedTasks.run();
                 default -> Navigation.EXIT;
             };
         }
@@ -24,6 +26,8 @@ public class PlanSync {
         System.out.println("2. Stopwatch");
         System.out.println("3. Time Calculator");
         System.out.println("4. Calendar");
+        System.out.println("5. Active Tasks");
+        System.out.println("6. Completed Tasks");
         System.out.println("0. Exit");
         System.out.print("\nChoose option: ");
 
@@ -32,6 +36,8 @@ public class PlanSync {
             case "2" -> Navigation.STOPWATCH;
             case "3" -> Navigation.TIME_CALCULATOR;
             case "4" -> Navigation.CALENDAR;
+            case "5" -> Navigation.ACTIVE_TASKS;
+            case "6" -> Navigation.COMPLETED_TASKS;
             case "0" -> Navigation.EXIT;
             default -> Navigation.MAIN;
         };
